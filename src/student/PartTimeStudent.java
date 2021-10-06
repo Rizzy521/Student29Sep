7a6ee990f48b230f29c90028fdc47245c9421383
rizwan hasan <rizzystar21@gmail.com>
2021-10-06 17:27:23 -0500

    adding new class called part time student

---
+++ src/student/PartTimeStudent.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package student;
+
+/**
+ *
+ * @author Riz
+ */
+public class PartTimeStudent extends Student {
+    
+    private int numOfCourses;
+
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+    
+    
+}
